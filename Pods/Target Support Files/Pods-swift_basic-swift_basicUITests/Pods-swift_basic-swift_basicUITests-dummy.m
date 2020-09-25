b70d606e51d2ee65d19c2a9b46dc7b29aa2426dd
Single Queen <enjoyming168@163.com>
2020-09-25 15:22:44 +0800

    初建项目： 1.swift基础框架 2.便于以后快速搭建项目 3.一些常用工具类的存储

---
+++ Pods/Target Support Files/Pods-swift_basic-swift_basicUITests/Pods-swift_basic-swift_basicUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_basic_swift_basicUITests : NSObject
+@end
+@implementation PodsDummy_Pods_swift_basic_swift_basicUITests
+@end
---
+++ swift_basic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_basic
+//
+//  Created by silence on 2020/9/25.
+//  Copyright © 2020 Silence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
